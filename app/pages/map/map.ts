106509b945a7fa63b5e6a883096411a887ded07f
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-07-03 11:59:27 +0600

    Insert map into layout

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -1,17 +1,52 @@
-import {Component} from '@angular/core';
-// import {leaflet} from 'leaflet/src';
+import {Component, OnInit, AfterViewChecked} from '@angular/core';
 
 @Component({
   templateUrl: 'build/pages/map/map.html'
 })
-export class MapPage
+export class MapPage implements OnInit, AfterViewChecked
 {
+  private _L: any;
+  private _map: any;
+
+  private _trackMapSize: boolean;
+
   constructor()
   {
-
+    this._trackMapSize = true;
   }
 
-  public openMenu ()
+  public ngOnInit (): void
+  {
+    this._L = window['L'];
+    const southWest = this._L.latLng(30, 10),
+          northEast = this._L.latLng(80, 200),
+          bounds = this._L.latLngBounds(southWest, northEast);
+    const startCoords = [54.908593335436926, 83.0291748046875];
+    const startZoom = 11;
+
+    this._map = this._L.map('map', {
+        minZoom: 4,
+        maxBounds: bounds
+    }).setView(startCoords, startZoom);
+window['mm'] = this._map;
+    this._L.tileLayer.provider('OpenStreetMap.HOT').addTo(this._map);
+
+    document.querySelector('.leaflet-control-zoom').remove();
+    document.querySelector('.leaflet-control-attribution').remove();
+  }
+
+  public ngAfterViewChecked (): void
+  {
+    // track when map container has been initialized
+    if (this._map._container.offsetWidth !== 0 && this._trackMapSize)
+    {
+      // map ready
+      this._trackMapSize = false;
+      this._map.invalidateSize();
+    }
+  }
+
+  public openMenu (): void
   {
     // location.hash = 'menu';
   }
