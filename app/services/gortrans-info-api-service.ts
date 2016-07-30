90cfb87ca4dd66c42500ff520b361e90250db11c
Yaroslav <jaroslav.dashkevich@gmail.com>
2016-07-30 23:01:03 +0700

    Fix: ip

--- app/services/gortrans-info-api-service.ts
+++ app/services/gortrans-info-api-service.ts
@@ -5,8 +5,8 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 
-const _apiRoot = 'http://192.168.1.157:3002';
-// const _apiRoot = 'http://nskgortrans.info';
+// const _apiRoot = 'http://192.168.1.157:3002';
+const _apiRoot = 'http://nskgortrans.info';
 
 @Injectable()
 export /**
